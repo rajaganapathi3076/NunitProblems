bf22eba82919d95ec65eb0f0e3095495e4bf0982
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-25 15:50:44 +0530

    adding UC1

---
+++ NunitProblems/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Welcome to the Nunit problems");
+            int[] notes = new int[] { 1000, 500, 100, 50, 20,10, 5, 2, 1 };
+            int amount = 0;
+            Console.WriteLine("Enter the amount:");
+            amount = Convert.ToInt32(Console.ReadLine());
+            int totalNotes = 0;
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    totalNotes += amount / notes[i];
+                    Console.WriteLine(notes[i] + " Rs Notes : " + amount / notes[i]);
+                    amount %= notes[i];
+                }
+            }
+            Console.WriteLine("Total Number of Notes : " + totalNotes);
+        }
+
+
+    }
+
+            
+        
+    
+}
---
+++ NunitProblems/VendingMachine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitProblems
+{
+    internal class VendingMachine
+    {
+
+        public static int[] GetNoteCount(int amount, int[] notes)
+        {
+            Parameter();
+            int[] noteCount = new int[notes.Length];
+            
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCount[i] = amount / notes[i];
+                    amount = amount % notes[i];
+                }
+            }
+            return noteCount;
+        }
+
+
+
+        public static void PrintNoteCount(int[] noteCount, int[] notes)
+        {
+            Console.WriteLine("Fewest number of notes to be returned:");
+
+            for (int i = 0; i < notes.Length; i++)
+            {
+                if (noteCount[i] > 0)
+                {
+                    Console.WriteLine($"{noteCount[i]} notes of {notes[i]}");
+                }
+
+            }
+
+
+        }
+        public static void Parameter()
+        {
+            int[] notes = { 100, 50, 20, 10, 5, 2, 1 };
+            int amount = 0;
+
+            Console.Write("Enter the amount: ");
+            amount = Convert.ToInt32(Console.ReadLine());
+
+            int[] noteCount = GetNoteCount(amount, notes);
+            PrintNoteCount(noteCount, notes);
+        } 
+        public static void Method()
+        {
+            Console.WriteLine("");
+        }
+        public static void Method1()
+        {
+            Console.WriteLine("");
+        }
+
+ }  }
+   
+    
+   
+
