a7823a61d434a6901d0ca042e6ea375a9f8316af
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-25 19:48:18 +0530

    adding UC4

--- NunitProblems/Program.cs
+++ NunitProblems/Program.cs
@@ -10,23 +10,29 @@ namespace NunitProblems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number in Celsius");
-            double c = Convert.ToDouble(Console.ReadLine());
-            
-            Console.WriteLine("Enter the number in infaherenheit ");
-            double f = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Welcome to the monthlypaments for EMI UC4");
+
+            Console.WriteLine("Please enter the Principal amount");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the how many year you want");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Rate of interest");
+            double R = Convert.ToDouble(Console.ReadLine());
 
 
-            double F = (c * 9 / 5) + 32;
-            double C = (f - 32) * 5 / 9;
-            Console.WriteLine("The temperature of celsius in {0} faherenheit {1}",c,F);
-            Console.WriteLine("The temperature of faherenheit in {0} celsius   {1}", f, C);
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+
+            double payment = (p * r) / (1 - Math.Pow(1 + r, -n));
+
+            Console.WriteLine("The total Monthly Payment {0}", payment);
+
+
+
+
         }
 
 
-    }
 
-            
-        
-    
-}
+
+}   }
