f6dd06e714a6fbcd09bea89529c55fff176f49ab
rajaganapathi3076 <rajaganapathi3076@gmail.com>
2023-05-28 11:47:23 +0530

    adding UC2

--- NunitProblems/Program.cs
+++ NunitProblems/Program.cs
@@ -6,49 +6,67 @@ using System.Threading.Tasks;
 
 namespace NunitProblems
 {
-    
+
     class program
     {
-        static string ToBinary(int number)
+        static void Main(string[] args)
         {
-            string binary=Convert.ToString(number,2);
-            return binary.PadLeft(8,'1');
+            Console.WriteLine("Welcome to the Day Of Week problem UC2");
+            Console.WriteLine("Please enter the value of year");
+            int y= int .Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the value of month ");
+            int m= int .Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the value of day");
+            int d= int .Parse(Console.ReadLine());
+            int result = 0;
 
-        }
-        static int SwapNibbles(int number)
-        {
-            return ((number & 0x0F) << 4) | ((number & 0x0F0) >> 4);
-        }
-        static bool IsPowerOfTwo(int number)
-        {
-            return(number !=0) &((number &(number -1))==0);
-        }
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Enter the Number:");
-            int number=Convert.ToInt32(Console.ReadLine());
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) - 2;
+            int d0 = (d + x + 31 * m0 / 12) % 7;
 
-            string binary=ToBinary(number);
-            Console.WriteLine("The binary Representation: "+  binary);
+            result = d0;
+            
+            switch (result)
+            {
+                case 0:
+                    result = 0;
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    result = 1;
+                    Console.WriteLine("Monday");
 
-            int swappednumber=SwapNibbles(number);
-            Console.WriteLine("After swapping Nibbles:  "+  swappednumber);
+                    break; 
+                case 2:
+                    result = 2;
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    result = 3;
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    result = 4;
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    result = 5;
+                    Console.WriteLine("Friday");
 
-            bool Ispoweroftwo = IsPowerOfTwo(number);
-            Console.WriteLine("Is the number a power of 2 ??  " + Ispoweroftwo);
+                    break;
+                case 6:
+                    result = 6;
+                    Console.WriteLine("Saturday");
+                    break;
+
+            }
+
+           
+            Console.WriteLine("The day of week is {0}", result); 
+            
 
 
         }
     }
-        
-    
-
-    
-    
-      
-
-
-
-
-
-}   
+}
